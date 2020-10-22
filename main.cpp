904d17d0516ae871f884f4dc01769ab9eb368661
Ruslan2808 <71653380+Ruslan2808@users.noreply.github.com>
2020-10-22 23:06:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
@@ -21,6 +22,9 @@ public:
 	~Array();
 };
 
+int menu(string, string);
+int getInt();
+
 int main() {
 	setlocale(LC_ALL, "rus");
 	srand(time(0));
@@ -30,45 +34,41 @@ int main() {
 		system("cls");
 		cout << "Введите размер 1-ой матрицы: " << endl;
 		cout << "Кол-во строк: ";
-		cin >> a;
+		a = getInt();
 		cout << "Кол-во столбцов: ";
-		cin >> b;
+		b = getInt();
 		Array mas1(a, b);
-		system("cls");
-		cout << "Введите элементы матрицы: " << endl;
-		cout << "Заполнение с клавиатуры - 1\nЗаполнение рандомными числами - любая другая цифра\n";
-		cin >> vib;
-		if (vib == 1) {
+		vib = menu("Заполнение с клавиатуры", "Заполнение рандомными числами");
+		if (vib == 0) {
+			cout << "Введите элементы матрицы: " << endl;
 			mas1.vvodKeyboard();
 		}
 		else {
 			cout << "Введите диапазон\n";
 			cout << "От: ";
-			cin >> min;
+			min = getInt();
 			cout << "До: ";
-			cin >> max;
+			max = getInt();
 			mas1.vvodRand(min, max);
 		}
 		system("cls");
 		cout << "Введите размер 2-ой матрицы: " << endl;
 		cout << "Кол-во строк: ";
-		cin >> a;
+		a = getInt();
 		cout << "Кол-во столбцов: ";
-		cin >> b;
+		b = getInt();
 		Array mas2(a, b);
-		system("cls");
-		cout << "Введите элементы матрицы: " << endl;
-		cout << "Заполнение с клавиатуры - 1\nЗаполнение рандомными числами - любая другая цифра\n";
-		cin >> vib;
-		if (vib == 1) {
+		vib = menu("Заполнение с клавиатуры", "Заполнение рандомными числами");
+		if (vib == 0) {
+			cout << "Введите элементы матрицы: " << endl;
 			mas2.vvodKeyboard();
 		}
 		else {
 			cout << "Введите диапазон\n";
 			cout << "От: ";
-			cin >> min;
+			min = getInt();
 			cout << "До: ";
-			cin >> max;
+			max = getInt();
 			mas2.vvodRand(min, max);
 		}
 		system("cls");
@@ -79,10 +79,8 @@ int main() {
 		result = mas1 + mas2;
 		system("pause");
 		system("cls");
-		cout << "Продолжить - 1\nВыход - любая другая цифра\n";
-		cin >> vib;
-		if (vib == 1) continue;
-		else break;
+		vib = menu("Продолжить", "Выйти");
+		if (vib != 0) break;
 	}
 	return 0;
 }
@@ -178,3 +176,59 @@ Array::~Array() {
 	delete[] mas;
 }
 
+int menu(string str1, string str2)
+{
+	int choiceButton = 0, button = 0;
+	system("cls");
+	while (true)
+	{
+		choiceButton = (choiceButton + 2) % 2;
+
+		if (choiceButton == 0) cout << "->" << str1 << endl;
+		else cout << str1 << endl;
+
+		if (choiceButton == 1) cout << "->" << str2 << endl;
+		else cout << str2 << endl;
+
+		button = _getch();
+		if (button == 224)
+		{
+			button = _getch();
+			if (button == 72) choiceButton--;
+			if (button == 80) choiceButton++;
+		}
+		if (button == 13) { 
+			system("cls"); 
+			return choiceButton; 
+		}
+
+		system("cls");
+	}
+}
+
+int getInt() {
+	string numbers;
+	while (true) {
+		int key;
+		key = _getch();
+		if (key == 224) {
+			key = _getch();
+		}
+		else
+			if (key == 8) {
+				if (numbers.length() != 0) {
+					cout << '\b' << " " << '\b';
+					numbers.erase(numbers.length() - 1);
+				}
+			}
+			else
+				if (key == 13) break;
+				else
+					if (key >= '0' && key <= '9' || key == '-') {
+						numbers = numbers + (char)key;
+						cout << (char)key;
+					}
+	}
+	cout << endl;
+	return stoi(numbers);
+}
